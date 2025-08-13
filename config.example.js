c1f0bbaab0490f65dcc70bc16bdabc3295ac01dd
kenis123 <kenismerekaf100@gmail.com>
2025-08-13 20:00:48 +0300

    Initial commit: Interactive massage quiz with Facebook Pixel and Telegram integration

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Приклад конфігурації
+// Скопіюйте цей файл як config.js і замініть на ваші токени
+
+window.TELEGRAM_BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE';
+window.TELEGRAM_CHAT_ID = 'YOUR_CHAT_ID_HERE'; 
